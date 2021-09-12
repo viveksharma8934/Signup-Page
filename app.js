2da31d956f7465d3690e44baa5875e5e8cb2103d
Viveksharma8934 <viveksharma8934@gmail.com>
2021-09-12 19:29:08 +0530

    signup page static css added

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const https = require("https");
 
 const app = express();
 
-app.use(express.static('internal'));
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/",function(req,res){
